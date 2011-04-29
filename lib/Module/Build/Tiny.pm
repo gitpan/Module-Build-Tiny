a9f0a068e61808b980fb92737f2bbbdd395d7158
Leon Timmermans <fawaka@gmail.com>
2011-04-29 08:57:49 +0000

    Import of LEONT/Module-Build-Tiny-0.006 from CPAN.
    gitpan-cpan-distribution: Module-Build-Tiny
    gitpan-cpan-version:      0.006
    gitpan-cpan-path:         LEONT/Module-Build-Tiny-0.006.tar.gz
    gitpan-cpan-author:       LEONT
    gitpan-cpan-maturity:     released

---
+++ lib/Module/Build/Tiny.pm
@@ -0,0 +1,190 @@
+package Module::Build::Tiny;
+BEGIN {
+  $Module::Build::Tiny::VERSION = '0.006';
+}
+use strict;
+use warnings;
+use Exporter 5.57 'import';
+our @EXPORT  = qw/Build Build_PL/;
+
+use CPAN::Meta;
+use ExtUtils::BuildRC qw/read_config/;
+use ExtUtils::Helpers qw/make_executable split_like_shell build_script manify man1_pagename man3_pagename/;
+use ExtUtils::Install qw/pm_to_blib install/;
+use ExtUtils::InstallPaths;
+use File::Path qw/rmtree/;
+use File::Find::Rule qw/find/;
+use File::Slurp qw/read_file write_file/;
+use File::Spec::Functions qw/catfile catdir rel2abs/;
+use Getopt::Long qw/GetOptions/;
+use JSON::PP qw/encode_json decode_json/;
+use TAP::Harness;
+
+my ($metafile) = grep { -e $_ } qw/META.json META.yml/ or die "No META information provided\n";
+my $meta = CPAN::Meta->load_file($metafile);
+
+sub _build {
+	my %opt = @_;
+	my @modules = find(file => name => [qw/*.pm *.pod/], in => 'lib');
+	my @scripts = find(file => name => '*', in => 'script');
+	pm_to_blib({ map { $_ => catfile('blib', $_) } @modules, @scripts }, catdir(qw/blib lib auto/));
+	make_executable($_) for find(file => in => catdir(qw/blib script/));
+	manify($_, catdir('blib', 'bindoc', man1_pagename($_)), 1, \%opt) for @scripts;
+	manify($_, catdir('blib', 'libdoc', man3_pagename($_)), 3, \%opt) for @modules;
+}
+
+my %actions = (
+	build => \&_build,
+	test  => sub {
+		my %opt = @_;
+		_build(%opt);
+		my $tester = TAP::Harness->new({verbosity => $opt{verbose}, lib => rel2abs(catdir(qw/blib lib/)), color => -t STDOUT});
+		$tester->runtests(sort +find(file => name => '*.t', in => 't'))->has_errors and exit 1;
+	},
+	install => sub {
+		my %opt = @_;
+		_build(%opt);
+		my $paths = ExtUtils::InstallPaths->new(%opt, module_name => $meta->name);
+		install($paths->install_map, @opt{'verbose', 'dry_run', 'uninst'});
+	},
+	clean => sub {
+		my %opt = @_;
+		rmtree('blib', $opt{verbose});
+	},
+	realclean => sub {
+		my %opt = @_;
+		rmtree($_, $opt{verbose}) for qw/blib Build _build_params MYMETA.yml MYMETA.json/;
+	},
+);
+
+sub Build {
+	my $bpl    = decode_json(read_file('_build_params'));
+	my $action = @ARGV && $ARGV[0] =~ /\A\w+\z/ ? shift @ARGV : 'build';
+	die "No such action '$action'\n" if not $actions{$action};
+	my $rc_opts = read_config();
+	my @env = defined $ENV{PERL_MB_OPT} ? split_like_shell($ENV{PERL_MB_OPT}) : ();
+	unshift @ARGV, map { @{$_} } grep { defined } $rc_opts->{'*'}, $bpl, $rc_opts->{$action}, \@env;
+	GetOptions(\my %opt, qw/install_base=s install_path=s% installdirs=s destdir=s prefix=s config=s% uninst:1 verbose:1 dry_run:1/);
+	$opt{config} = ExtUtils::Config->new($opt{config});
+	$actions{$action}->(%opt);
+}
+
+sub Build_PL {
+	printf "Creating new 'Build' script for '%s' version '%s'\n", $meta->name, $meta->version;
+	my $dir = $meta->name eq 'Module-Build-Tiny' ? 'lib' : 'inc';
+	write_file(build_script(), "#!perl\nuse lib '$dir';\nuse Module::Build::Tiny;\nBuild();\n");
+	make_executable(build_script());
+	write_file(qw/_build_params/, encode_json(\@ARGV));
+	write_file("MY$_", read_file($_)) for grep { -f } qw/META.json META.yml/;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Module::Build::Tiny - A tiny replacement for Module::Build
+
+=head1 SYNOPSIS
+
+ use Module::Build::Tiny;
+ BuildPL();
+
+=head1 DESCRIPTION
+
+Many Perl distributions use a Build.PL file instead of a Makefile.PL file
+to drive distribution configuration, build, test and installation.
+Traditionally, Build.PL uses Module::Build as the underlying build system.
+This module provides a simple, lightweight, drop-in replacement.
+
+Whereas Module::Build has over 6,700 lines of code; this module has under
+100, yet supports the features needed by most pure-Perl distributions.
+
+=head2 Supported
+
+  * Pure Perl distributions
+  * Recursive test files
+  * MYMETA
+  * Man page generation
+
+=head2 Not Supported
+
+  * Dynamic prerequisites
+  * Generated code from PL files
+  * Building XS or C
+  * HTML documentation generation
+  * Extending Module::Build::Tiny
+
+=head2 Other limitations
+
+  * This is an experimental module -- use at your own risk
+
+=head2 Directory structure
+
+Your .pm and .pod files must be in F<lib/>.  Any executables must be in
+F<script/>.  Test files must be in F<t/>.  Bundled test modules must be in
+F<t/lib/>.
+
+=head1 USAGE
+
+These all work pretty much like their Module::Build equivalents.
+
+=head2 perl Build.PL
+
+=head2 Build [ build ] 
+
+=head2 Build test
+
+=head2 Build install
+
+This supports the following options:
+
+=over
+
+=item * install_base
+
+=item * installdirs
+
+=item * prefix
+
+=item * install_path
+
+=item * destdir
+
+=item * uninst
+
+=back
+
+=head2 Build clean
+
+=head2 Build realclean
+
+=head1 AUTHORING
+
+This module doesn't support authoring. To develop modules using Module::Build::Tiny, usage of L<Dist::Zilla::Plugin::ModuleBuildTiny> is recommended.
+
+=head1 CONFIG FILE AND ENVIRONMENT
+
+Options can be provided in a F<.modulebuildrc> file or in the C<PERL_MB_OPT>
+environment variable the same way they can with Module::Build.
+
+=head1 SEE ALSO
+
+L<Module::Build>
+
+=head1 AUTHOR
+
+  David Golden <dagolden@cpan.org>
+  Leon Timmermans <leont@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2009 - 2011 by David A. Golden, Leon Timmermans
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
+# vi:et:sts=2:sw=2:ts=2
---
+++ t/simple.t
@@ -0,0 +1,82 @@
+use strict;
+use warnings;
+use Config;
+use File::pushd 1.00 qw(tempd);
+use File::Spec 0 ();
+use Capture::Tiny 0 qw(capture);
+use Test::More 0.86;
+use lib 't/lib';
+use DistGen qw/undent/;
+
+plan tests => 16;
+
+#--------------------------------------------------------------------------#
+# fixtures
+#--------------------------------------------------------------------------#
+
+my $dist = DistGen->new(name => 'Foo::Bar');
+$dist->chdir_in;
+$dist->add_file('script/simple', undent(<<"    ---"));
+    #!perl
+    use Foo::Bar;
+    print Simple->VERSION . "\n";
+    ---
+$dist->regen;
+
+my $interpreter = ($Config{startperl} eq $^X )
+                ? qr/#!\Q$^X\E/
+                : qr/(?:#!\Q$^X\E|\Q$Config{startperl}\E)/;
+my ($guts, $ec);
+
+sub _mod2pm   { (my $mod = shift) =~ s{::}{/}g; return "$mod.pm" }
+sub _path2mod { (my $pm  = shift) =~ s{/}{::}g; return substr $pm, 5, -3 }
+sub _mod2dist { (my $mod = shift) =~ s{::}{-}g; return $mod; }
+sub _slurp { do { local (@ARGV,$/)=$_[0]; <> } }
+
+#--------------------------------------------------------------------------#
+# configure
+#--------------------------------------------------------------------------#
+
+{
+  ok( ! system($^X, "Build.PL"), "Ran Build.PL");
+  ok( -f 'Build' && -x _, "Build created and executable" );
+
+  open my $fh, "<", "Build";
+  my $line = <$fh>;
+
+  like( $line, qr{\A$interpreter}, "Build has shebang line with \$^X" );
+  ok( -f '_build_params', "_build_params created" );
+}
+
+#--------------------------------------------------------------------------#
+# build
+#--------------------------------------------------------------------------#
+
+{
+  ok( capture { ! system($^X, "Build") }, "Ran Build");
+  ok( -d 'blib',        "created blib" );
+  ok( -d 'blib/lib',    "created blib/lib" );
+  ok( -d 'blib/script', "created blib/script" );
+
+  # check pm
+  my $pmfile = _mod2pm($dist->name);
+  ok( -f 'blib/lib/' . $pmfile, "$dist->{name} copied to blib" );
+  is( _slurp("lib/$pmfile"), _slurp("blib/lib/$pmfile"), "pm contents are correct" );
+  ok( ! -w "blib/lib/$pmfile", "pm file in blib is readonly" );
+
+  # check bin
+  ok( -f 'blib/script/simple', "bin/simple copied to blib" );
+  like( _slurp("blib/script/simple"), '/' .quotemeta(_slurp("blib/script/simple")) . "/",
+    "blib/script/simple contents are correct" );
+  {
+    local $TODO = 'What to do about this?';
+    ok( ! -w "blib/script/simple", "blib/script/simple is readonly" );
+  }
+  ok( -x "blib/script/simple", "blib/script/simple is executable" );
+  open my $fh, "<", "blib/script/simple";
+  my $line = <$fh>;
+  like( $line, qr{\A$interpreter}, "blib/script/simple has shebang line with \$^X" );
+
+}
+
+

efd486ed733c437e332e192548a22642cb6ce465
Leon Timmermans <fawaka@gmail.com>
2014-01-22 02:43:39 +0000

    Import of LEONT/Module-Build-Tiny-0.034 from CPAN.
    gitpan-cpan-distribution: Module-Build-Tiny
    gitpan-cpan-version:      0.034
    gitpan-cpan-path:         LEONT/Module-Build-Tiny-0.034.tar.gz
    gitpan-cpan-author:       LEONT
    gitpan-cpan-maturity:     released

--- lib/Module/Build/Tiny.pm
+++ lib/Module/Build/Tiny.pm
@@ -1,5 +1,5 @@
 package Module::Build::Tiny;
-$Module::Build::Tiny::VERSION = '0.033';
+$Module::Build::Tiny::VERSION = '0.034';
 use strict;
 use warnings;
 use Exporter 5.57 'import';
@@ -160,7 +160,7 @@ Module::Build::Tiny - A tiny replacement for Module::Build
 
 =head1 VERSION
 
-version 0.033
+version 0.034
 
 =head1 SYNOPSIS
 
